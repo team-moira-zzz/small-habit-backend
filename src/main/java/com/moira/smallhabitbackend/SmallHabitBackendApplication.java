175d1d341cddf773a36ba515934b7bb9ade286d2
hunzzzzz <hun819.st@gmail.com>
2025-09-22 20:50:34 +0900

    초기 커밋

---
+++ src/main/java/com/moira/smallhabitbackend/SmallHabitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moira.smallhabitbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallHabitBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallHabitBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moira/smallhabitbackend/SmallHabitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moira.smallhabitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallHabitBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
